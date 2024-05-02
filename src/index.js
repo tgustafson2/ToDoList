fce09ab2e3f8fe7eae4315a408399d8b57fae3b1
Thomas Gustafson <gus.duckfan@gmail.com>
2024-05-01 20:32:10 -0700

    Setups up project structure and lays out localstorage design

---
+++ src/index.js
@@ -0,0 +1,30 @@
+
+/*
+local storage design
+
+to-do-lists: {
+    categories: [
+        {
+            categoryName: "",
+            categoryId: "",
+            lists:[
+                {
+                    listName: name,
+                    listId: id,
+                    items: [{
+                        itemTitle: "",
+                        description: "",
+                        notes: "",
+                        dueDateTime: "",
+                        done: "",
+                        itemId: "",
+                        importance: "",}]
+                }
+            ]
+        }
+    ]
+}
+
+
+*/
+
